63f3fd60498a485c6abea488141c4b603c8ecab5
Jemena <87300836+Jemena@users.noreply.github.com>
2021-08-17 20:41:25 +0200

    Add files via upload

---
+++ TFC/libreria/settings.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+import pygame
+
+
+#COLOURS AND WINDOW SETTINGS
+
+pygame.init()
+pygame.font.init()
+
+WIDTH, HEIGHT = 600, 700       # SCREEN
+
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+GREY = [128, 128, 128]
+BLUE = [0, 0, 255]
+GREEN = [0, 255, 0]
+RED = [255, 0, 0]
+TEXTCOLOR = [0, 0, 0]
+GRAY = GREY
+
+FPS = 240                # Limitar velocidad del programa y dibujo
+
+ROWS = COLS = 100         #Resolucion
+
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+PIXEL_SIZE = WIDTH // COLS
+
+BG_COLOR = WHITE              # Background
+
+DRAW_GRID_LINES = False            # Mejor sin que se vean
+
+DATAFILE = 'paint.json'
+
+
+def get_font(size):
+    return pygame.font.SysFont("comicsans", size)    # Fuente de texto
+
+
+
